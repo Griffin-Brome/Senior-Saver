0548cc558743ae67afd34687f3eba05601f310bc
griffin <griffinbrome@gmail.com>
2020-01-19 05:05:47 -0800

    location data and user id is received by server

--- twilio/app.py
+++ twilio/app.py
@@ -1,13 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import sms
 
 app = Flask(__name__)
 
-@app.route("/help")
+@app.route("/help", methods=["POST", "GET"])
 def help():
     sms.send_sms()
+    lon = request.args.get("lon")
+    lat = request.args.get("lat")
+    user_id = request.args.get("uid")
+    print (str(lon), str(lat), str(user_id))
     return "Message Sent"
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
