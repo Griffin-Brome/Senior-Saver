33e4beddc8ae8a466fede94e58782deaeba45b65
Nate <natewick005@gmail.com>
2020-01-19 01:00:41 -0800

    Latest
    Vitamin c keeps u going

--- android/app/src/main/java/com/example/seniorsaver/MainActivity.java
+++ android/app/src/main/java/com/example/seniorsaver/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.seniorsaver;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
@@ -16,77 +17,95 @@ import androidx.core.content.ContextCompat;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
+import org.w3c.dom.Text;
+
+import java.time.LocalDateTime;
 
 import io.radar.sdk.Radar;
 import io.radar.sdk.model.RadarEvent;
 import io.radar.sdk.model.RadarUser;
 
 public class MainActivity extends AppCompatActivity {
-private static final int ACCESS_FINE_LOCATION_CODE = 100;
-private static final int ACCESS_BACKGROUND_LOCATION = 101;
+private static final String userId = "AppTest";
+private static final String publishableKey = "prj_test_pk_9a3d304c66917becaf457cdf475503827f9ec36c";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        //Initialising RADAR
-        String publishableKey = "prj_test_pk_9a3d304c66917becaf457cdf475503827f9ec36c";
-        Radar.initialize(publishableKey);
-        String userId = "AppTest";
-        Radar.setUserId(userId);
+        setContentView(R.layout.activity_main);
 
-        //Requesting Permissions
-        checkpermissions(Manifest.permission.ACCESS_FINE_LOCATION,ACCESS_FINE_LOCATION_CODE);
-        checkpermissions(Manifest.permission.ACCESS_BACKGROUND_LOCATION,ACCESS_BACKGROUND_LOCATION);
-
-
-
+        //Initialise RADAR
+        initialise();
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        Button helpBtn = (Button) findViewById(R.id.gpsBtn);
+        helpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                help();
+            }
+        });
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+
+    //Sets key and userId, checks permissions
+    public void initialise(){
+        Radar.initialize(publishableKey);
+        Radar.setUserId(userId);
+        permissions();
+        String[] permissions = new String[] {Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_BACKGROUND_LOCATION};
+
+        if (!hasPermissions(this, permissions)) {
+            ActivityCompat.requestPermissions(this, permissions, 1);
+        }
+
+    }
+
+    public void permissions(){
+        String[] permissions = new String[] {Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_BACKGROUND_LOCATION};
+
+        if (!hasPermissions(this, permissions)) {
+            ActivityCompat.requestPermissions(this, permissions, 2);
+        }
+
+    }
+
+    //Used to check permissions
+    public static boolean hasPermissions(Context context, String... permissions) {
+        if (context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-    public void checkpermissions(String permission, int requestCode) {
-        if (ContextCompat.checkSelfPermission(MainActivity.this,
-                permission)
-                == PackageManager.PERMISSION_DENIED) {
-            //Permission is not granted
-            ActivityCompat.requestPermissions(MainActivity.this, new String[]{permission}, requestCode);
-
-        }
-        //ActivityCompat.requestPermissions(MainActivity.this, new String[] { Manifest.permission.ACCESS_FINE_LOCATION }, requestCode);
-
-    }
     public void locate() {
         Radar.trackOnce(new Radar.RadarCallback() {
             @Override
             public void onComplete(@NotNull Radar.RadarStatus radarStatus, @Nullable Location location, @Nullable RadarEvent[] radarEvents, @Nullable RadarUser radarUser) {
-
+            //RadarEvent event = new RadarEvent();
+                //myEvent event = new myEvent(location.getLongitude(), location.getLatitude());
+                TextView view = (TextView) findViewById(R.id.textView3);
+                view.setText("" + location.getLongitude() + " " + location.getLatitude() + " " + userId + " " + LocalDateTime.now());
             }
         });
+    }
+
+
+    public void help(){
+        locate();
     }
 }
